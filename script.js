5b2c78469243c955fa1fa2231c1da4c295db5744
BenGavieres <benjamin.gavieres@gmail.com>
2023-07-09 17:50:04 -0400

    Added a method that simulates one round.

--- script.js
+++ script.js
@@ -1,10 +1,49 @@
-
+//Returns a random integer from specified bounds
 function getRandomInt(min,max) {
     return Math.floor(Math.random()*(max+1))+min;
 }
 
-//Chooses a random option from an array of 3 elements
+//Returns a random option from an array of 3 elements
 function getComputerChoice() {
-    const computerChoice=["Rock","Paper","Scissors"];
+    const computerChoice=["rock","paper","scissors"];
     return computerChoice[getRandomInt(0,2)];
+}
+
+function logLoss(player,comp) {
+    console.log(`You lose! ${comp} beats ${player}`);
+}
+
+function logWin(player,comp) {
+    console.log(`You win! ${player} beats ${comp}`);
+}
+
+//Simulates one round
+function playRound(playerSelection,computerSelection) {
+    if (playerSelection==computerSelection){
+        console.log(`Draw! You both chose ${playerSelection}`);
+    }
+    else if (playerSelection=="rock"){
+        if (computerSelection=="paper") {
+            logLoss(playerSelection,computerSelection);
+        }
+        else {
+            logWin(playerSelection,computerSelection);
+        }
+    }
+    else if (playerSelection=="paper") {
+        if (computerSelection=="rock") {
+            logWin(playerSelection,computerSelection);
+        }
+        else {
+            logLoss(playerSelection,computerSelection);
+        }
+    }
+    else {
+        if (computerSelection=="rock") {
+            logWin(playerSelection,computerSelection);
+        }
+        else {
+            logLoss(playerSelection,computerSelection);
+        }
+    }
 }
